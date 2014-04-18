63b9417bcf6845d80984287938a2ed2a2add34f7
Stepan Koltsov <stepan.koltsov@gmail.com>
2014-04-18 17:31:57 +0000

    simpler version of mutex
    that uses single int

--- mutex.c
+++ mutex.c
@@ -13,20 +13,20 @@
 
 // util
 
-static inline int futex(int *uaddr, int op, int val) {
+static inline unsigned futex(unsigned *uaddr, unsigned op, unsigned val) {
     return syscall(SYS_futex, uaddr, op, val, 0, 0, 0);
 }
 
-static inline bool atomic_compare_exchange(int* ptr, int compare, int exchange) {
+static inline bool atomic_compare_exchange(unsigned* ptr, unsigned compare, unsigned exchange) {
     return __atomic_compare_exchange_n(ptr, &compare, exchange,
             0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
 }
 
-static inline void atomic_store(int* ptr, int value) {
-    __atomic_store_n(ptr, 0, __ATOMIC_SEQ_CST);
+static inline unsigned atomic_load(unsigned* ptr) {
+    return __atomic_load_n(ptr, __ATOMIC_SEQ_CST);
 }
 
-static inline int atomic_add_fetch(int* ptr, int d) {
+static inline unsigned atomic_add_fetch(unsigned* ptr, unsigned d) {
     return __atomic_add_fetch(ptr, d, __ATOMIC_SEQ_CST);
 }
 
@@ -34,27 +34,68 @@ static inline int atomic_add_fetch(int* ptr, int d) {
 // mutex library
 
 struct mutex {
-    // 0 unlocked
-    // 1 locked
-    int locked;
-    // number of threads requesting a lock
-    int count;
+    // high bit:   locked
+    // other bits: number of clients
+    unsigned locked;
 };
 
-#define MUTEX_INIT { 0, 0 };
+#define MUTEX_INIT { 0 };
+
+static const unsigned LOCKED_MASK = 1 << (sizeof(unsigned) * 8 - 1);
 
 void mutex_lock(struct mutex* mutex) {
-    atomic_add_fetch(&mutex->count, 1);
-    while (!atomic_compare_exchange(&mutex->locked, 0, 1)) {
-        futex(&mutex->locked, FUTEX_WAIT, 1);
+    // fast track
+    if (atomic_compare_exchange(&mutex->locked, 0, 1 | LOCKED_MASK)) {
+        return;
+    }
+
+    // increment number of waiters, keep locked flag
+    for (;;) {
+        unsigned v = atomic_load(&mutex->locked);
+        unsigned locked = v & LOCKED_MASK;
+        unsigned n = v & ~LOCKED_MASK;
+        // assert we are not leaking
+        if (n > 10000) {
+            abort();
+        }
+        if (atomic_compare_exchange(&mutex->locked, v, (n + 1) | locked)) {
+            break;
+        }
+    }
+
+    // do lock
+    for (;;) {
+        unsigned v = atomic_load(&mutex->locked);
+        if (v & LOCKED_MASK) {
+            // locked by someone else, wait
+            futex(&mutex->locked, FUTEX_WAIT, v);
+        } else {
+            // try lock
+            if (atomic_compare_exchange(&mutex->locked, v, v | LOCKED_MASK)) {
+                break;
+            }
+        }
     }
 }
 
 void mutex_unlock(struct mutex* mutex) {
-    int left = atomic_add_fetch(&mutex->count, -1);
-    atomic_store(&mutex->locked, 0);
-    if (left > 0) {
-        futex(&mutex->locked, FUTEX_WAKE, 1);
+    // fast track
+    if (atomic_compare_exchange(&mutex->locked, 1 | LOCKED_MASK, 0)) {
+        return;
+    }
+
+    // unlock
+    for (;;) {
+        unsigned v = atomic_load(&mutex->locked);
+        if (!(v & LOCKED_MASK)) {
+            // assert
+            abort();
+        }
+        unsigned n = v & ~LOCKED_MASK;
+        if (atomic_compare_exchange(&mutex->locked, v, n - 1)) {
+            futex(&mutex->locked, FUTEX_WAKE, 1);
+            return;
+        }
     }
 }
 
@@ -64,14 +105,14 @@ void mutex_unlock(struct mutex* mutex) {
 struct my_thread_params {
     int n;
     struct mutex* lock;
-    int* lock_count;
+    unsigned* lock_count;
 };
 
 void* run_my_thread(void* param) {
     struct my_thread_params* my = (struct my_thread_params*) param;
     for (;;) {
         mutex_lock(my->lock);
-        if (atomic_add_fetch(my->lock_count, 1) > 1) {
+        if (atomic_add_fetch(my->lock_count, 1u) > 1u) {
             fprintf(stderr, "lock is broken\n");
             exit(1);
         }
@@ -84,7 +125,7 @@ void* run_my_thread(void* param) {
 
 int main() {
     struct mutex lock = MUTEX_INIT;
-    int lock_count = 0;
+    unsigned lock_count = 0;
     static const int nthreads = 5;
     pthread_t threads[nthreads];
     struct my_thread_params params[nthreads];
